93014086757a38d241cb63145d4292ede8f55a2a
Alan Alpert <alan.alpert@bluescape.com>
2022-11-21 18:31:32 +0530

    nac_2 WIP
    Still needs game logic in rust and player selection

--- nac_2/src/main.rs
+++ nac_2/src/main.rs
@@ -2,32 +2,44 @@ use slint;
 
 slint::slint! {
     GameTile := Rectangle {
+        callback clicked;
+        property <int> player;
         width: 64px;
         height: 64px;
         background: lightsteelblue;
         Image {
-            source: @image-url("assets/X.png");
+            source: player == 1  ?  @image-url("assets/X.png")
+                : player == 2 ? @image-url("assets/O.png")
+                : @image-url("assets/empty.png");
             width: parent.width;
             height: parent.height;
+        }
+
+        TouchArea {
+            clicked => {
+                root.clicked();
+            }
         }
     }
 
     MainWindow := Window {
-        property <[int]> tile_data: [
-            1,2,3,4,5,6,7,8,9
-        ];
-
+        property <[int]> tile_data;
         for tile[i] in tile_data : GameTile {
             x: mod(i, 3) * 74px;
             y: floor(i / 3) * 74px;
+            player: tile;
+            clicked => {
+                tile += 1;
+            }
+
         }
     }
 }
 
 fn main() {
     let main_window = MainWindow::new();
-    //let tile_data = vec![0,0,0,0,0,0,0,0,0];
-    //main_window.set_tile_data(tile_data.into())
+    let tile_data = std::rc::Rc::new(slint::VecModel::from(vec![0,0,0,0,0,0,0,0,0]));
+    main_window.set_tile_data(tile_data.into());
     main_window.run();
 }
 
